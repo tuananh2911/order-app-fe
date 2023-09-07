d9f8b1cb9662715c233e1684d4198d8448a6516c
tuananhsonat <anhlt@sonat.vn>
2023-09-07 14:13:01 +0700

    feat:init app

---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import { FruitsSection, MenuSection, ShowcaseBanner } from "../../components";
+
+const Home = () => {
+  return (
+    <div className="flex w-full h-auto flex-col items-center justify-center">
+      <FruitsSection />
+      <MenuSection />
+    </div>
+  );
+};
+
+export default Home;
